a103fbccd3a44157d0f2f5590f7c0b7e9a3906ae
Lucas Viana <lucasvfv@gmail.com>
2025-10-21 19:53:59 -0300

    Adicionar arquivos de projeto.

---
+++ Academico2025/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Academico2025.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Aula()
+        {
+            return View();
+        }
+    }
+}
